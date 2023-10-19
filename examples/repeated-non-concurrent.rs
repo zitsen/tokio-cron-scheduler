287e7a21ff47e21cd8501bb471ff175c4c17784f
Huo Linhe <linhehuo@gmail.com>
2023-10-19 22:49:30 +0800

    chore: do dot schedule next tick when last tick is running

--- examples/repeated-non-concurrent.rs
+++ examples/repeated-non-concurrent.rs
@@ -20,13 +20,21 @@ async fn main() {
 
     let job_run_times = Arc::new(AtomicUsize::new(0));
     let job_run_times_in_job = job_run_times.clone();
-    let job = Job::new_repeated_async(Duration::from_secs(4), move |_uuid, _l| {
+    let job = Job::new_repeated_async(Duration::from_secs(2), move |_uuid, mut _l| {
         let job_run_times_in_job = job_run_times_in_job.clone();
         Box::pin(async move {
             info!("I'm repeated async every 4 seconds");
             tokio::time::sleep(Duration::from_secs(7)).await;
             info!("I'm repeated async every 4 seconds but we sleep 7 seconds");
             job_run_times_in_job.fetch_add(1, std::sync::atomic::Ordering::AcqRel);
+            match _l.next_tick_for_job(_uuid).await {
+                Ok(val) => {
+                    info!("Next tick for job {:?} is {:?}", _uuid, val);
+                }
+                Err(e) => {
+                    info!("Error getting next tick for job {:?} {:?}", _uuid, e);
+                }
+            }
         })
     })
     .unwrap();
